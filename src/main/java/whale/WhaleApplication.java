8f7c1d7d8df6172a1d46749b7d21591066846fe1
Umboon <uo_oom@hotmail.com>
2015-05-23 12:01:50 +0700

    initail project

---
+++ src/main/java/whale/WhaleApplication.java
@@ -0,0 +1,12 @@
+package whale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhaleApplication.class, args);
+    }
+}
---
+++ src/test/java/whale/WhaleApplicationTests.java
@@ -0,0 +1,18 @@
+package whale;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WhaleApplication.class)
+@WebAppConfiguration
+public class WhaleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
